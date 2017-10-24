19c062e5e885f1d92a8c0808a8ec56f00e0cb05a
onocy <michalowski.phillip@gmail.com>
2017-10-24 13:48:49 -0400

    structure for models

--- cleanslate/doma/models.py
+++ cleanslate/doma/models.py
@@ -6,20 +6,26 @@ class User(models.Model):
     status = models.TextField(max_length=1000, help_text='Enter a status for others to view')
     lastSeen = models.DateField(null=True, blank=True)
     email = models.CharField(max_length=100)
-    # password
-    # phone
+
+    # make hidden 
+    password = models.CharField(max_length=100, help_text='Enter your password')
+    
+    phone = models.CharField(max_length=10, help_text='Enter your phone number')
     bio = models.TextField(max_length=1000, help_text='Enter a brief description of yourself')
-    # class
+    # may not be necessary? 
+    yog = models.CharField(max_length=100, help_text='Enter your graduation date')
     major = models.CharField(max_length=100)
-    # homerole
+
+    # fix select option
+    homerole = models.CharField(max_length=1)
 
     # uncomment out home when Home model is declared
     home = models.OneToOneField('Home', on_delete=models.SET_NULL, null=True)
 
     # reviews, forums, topics, and posts are one to many relations, so user will be foreign key in those models
     smokes = models.BooleanField(default=False)
-    # bedtime
-    # likesAnimals
+    bedtime = models.TimeField()
+    likesAnimals = models.NullBooleanField()
 
     def get_absolute_url(self):
         return reverse('user-detail', args=[str(self.id)])
@@ -36,5 +42,74 @@ class Home(models.Model):
     # village = models.OneToOneField('Village', on_delete=models.SET_NULL, null=True)
     # forum = models.OneToOneField('Forum', on_delete=models.SET_NULL, null=True)
 
+    def get_absolute_url(self):
+        return reverse('home-detail', args=[str(self.id)])
+
     def __str__(self):
         return self.name
+
+
+class Reviews(models.Model):
+    # Define fields
+
+
+    # Define Methods
+    '''
+    def get_absolute_url(self):
+        return reverse('home-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.name
+    '''
+
+class Forum(models.Model):
+    # Define fields
+
+
+    # Define Methods
+    '''
+    def get_absolute_url(self):
+        return reverse('home-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.name
+    '''
+
+class Topic(models.Model):
+    # Define fields
+
+
+    # Define Methods
+    '''
+    def get_absolute_url(self):
+        return reverse('home-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.name
+    '''
+
+class Post(models.Model):
+    # Define fields
+
+
+    # Define Methods
+    '''
+    def get_absolute_url(self):
+        return reverse('home-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.name
+    '''
+
+class Village(models.Model):
+    # Define fields
+
+
+    # Define Methods
+    '''
+    def get_absolute_url(self):
+        return reverse('home-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.name
+    '''
