6d05809f9a9329b69d91b335b798836ef21bbc4b
Phillip Michalowski <onocy@users.noreply.github.com>
2017-10-24 14:10:46 -0400

    Update admin.py

--- cleanslate/doma/admin.py
+++ cleanslate/doma/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from .models import User
 from .models import Home
+from .models import Reviews
+from .models import Forum
 from .models import Topic
+from .models import Post
 from .models import Village
 
 
 admin.site.register(User)
 admin.site.register(Home)
+admin.site.register(Reviews)
+admin.site.register(Forum)
 admin.site.register(Topic)
+admin.site.register(Post)
 admin.site.register(Village)
