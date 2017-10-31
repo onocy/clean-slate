e2c1fe30b00ca6eac11bed579b465eaa9085cace
onocy <michalowski.phillip@gmail.com>
2017-10-31 14:34:20 -0400

    resolved issue w/ pet allergies. added additional model classes for data

--- cleanslate/doma/models.py
+++ cleanslate/doma/models.py
@@ -5,17 +5,18 @@ from django.urls import reverse
 class User(models.Model):
     # reviews, forums, topics, and posts are one to many relations, so user will be foreign key in those models
     role_choices = (
-        ('a', 'admin'),
-        ('u', 'user')
+        ('a', 'Admin'),
+        ('u', 'User')
     )
     # make hidden
     password = models.CharField(max_length=100, help_text='Enter your password', null=True, blank=True)
+
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     phone = models.CharField(max_length=10, help_text='Enter your phone number', null=True, blank=True)
     yog = models.CharField(max_length=100, help_text='Enter your graduation date', null=True, blank=True)
     major = models.CharField(max_length=100, null=True, blank=True)
-    # role = models.CharField(max_length=1, choices=role_choices, default='u')
+    role = models.CharField(max_length=1, choices=role_choices, default='u')
 
     status = models.TextField(max_length=1000, help_text='Enter a status for others to view')
     bio = models.TextField(max_length=1000, help_text='Enter a brief description of yourself')
@@ -24,7 +25,7 @@ class User(models.Model):
     lastSeen = models.DateField(null=True, blank=True)
     email = models.EmailField(help_text='Enter your email')
 
-    # pet_allergies = models.NullBooleanField(null=True, blank=True, help_text='Are you allergic to pets?')
+    pet_allergies = models.NullBooleanField(null=True, blank=True, help_text='Are you allergic to pets?')
     home = models.OneToOneField('Home', on_delete=models.SET_NULL, null=True, related_name='user_home')
 
     def is_admin(self):
@@ -54,7 +55,7 @@ class Home(models.Model):
 
 
 class Topic(models.Model):
-    # title = models.CharField(max_length=200, help_text="Enter a topic name")
+    title = models.CharField(max_length=200, help_text="Enter a topic name")
     content = models.CharField(max_length=500)
     forum = models.ForeignKey('Forum', on_delete=models.CASCADE, null=False, primary_key=True)
     created_by = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
@@ -92,7 +93,7 @@ class Review(models.Model):
 
 class Forum(models.Model):
     title = models.CharField(max_length=200, help_text="Enter a forum name")
-    #desc?
+    description = models.TextField(max_length=1000, help_text='Enter a description for this forum')
     created_by = models.ForeignKey('User', on_delete=models.CASCADE, null=False, primary_key=True)
     created_on = models.DateField()
 
@@ -107,13 +108,53 @@ class Post(models.Model):
     title = models.CharField(max_length=200, help_text='Enter a post name')
     content = models.CharField(max_length=500, help_text='Enter content')
     topic = models.ForeignKey('Topic', on_delete=models.SET_NULL, null=True)
-    # What is this one? -michalo
     # posts = models.ForeignKey('Post', on_delete=models.SET_NULL, null=True)
     created_by = models.ForeignKey('User', on_delete=models.CASCADE, null=False, primary_key=True, related_name='op')
     created_on = models.DateTimeField()
-    
+
     def __str__(self):
         return 'Post: %s' % self.title
 
     def get_absolute_url(self):
         return reverse('post-detail', args=[str(self.id)])
+
+
+class Transaction(models.Model):
+    title = models.CharField(max_length=200, help_text='Enter a transaction name')
+
+    def __str__(self):
+        return 'Transaction: %s' % self.title
+
+    def get_absolute_url(self):
+        return reverse('transaction-detail', args=[str(self.id)])
+
+
+class Chore(models.Model):
+    title = models.CharField(max_length=200, help_text='Enter a chore name')
+
+    def __str__(self):
+        return 'Chore: %s' % self.title
+
+    def get_absolute_url(self):
+        return reverse('chore-detail', args=[str(self.id)])
+
+
+class Reminder(models.Model):
+    title = models.CharField(max_length=200, help_text='Enter a reminder name')
+
+    def __str__(self):
+        return 'Reminder: %s' % self.title
+
+    def get_absolute_url(self):
+        return reverse('reminder-detail', args=[str(self.id)])
+
+
+# Will we be updating the calendar w/ saved data?
+class Event(models.Model):
+    title = models.CharField(max_length=200, help_text='Enter an event name')
+
+    def __str__(self):
+        return 'Event: %s' % self.title
+
+    def get_absolute_url(self):
+        return reverse('event-detail', args=[str(self.id)])
