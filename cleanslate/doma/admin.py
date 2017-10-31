4eb808171a90dab906c6cefca1ddc2ce4317dc6f
onocy <michalowski.phillip@gmail.com>
2017-10-31 14:45:25 -0400

    Added new models to admin file

--- cleanslate/doma/admin.py
+++ cleanslate/doma/admin.py
@@ -7,6 +7,10 @@ from .models import Forum
 from .models import Topic
 from .models import Post
 from .models import Village
+from .models import Transaction
+from .models import Chore
+from .models import Reminder
+from .models import Event
 
 
 admin.site.register(User)
@@ -16,3 +20,7 @@ admin.site.register(Forum)
 admin.site.register(Topic)
 admin.site.register(Post)
 admin.site.register(Village)
+admin.site.register(Transaction)
+admin.site.register(Chore)
+admin.site.register(Reminder)
+admin.site.register(Event)
