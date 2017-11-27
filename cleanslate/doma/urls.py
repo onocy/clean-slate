2cd058e473ce1b6ab3b6a486f1966f40f56c9231
Molly O'Neil <mboneil@umass.edu>
2017-11-27 18:41:44 -0500

    Url for editing a chore

--- cleanslate/doma/urls.py
+++ cleanslate/doma/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^chore/(?P<pk>[-\w]+)/edit/$', views.edit_chore_deadline, name = 'edit-chore-deadline'),
 ]
