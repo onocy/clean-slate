68f2f93fb5e61feb2c073b6ce3a94ad0227a1e96
Molly O'Neil <mboneil@umass.edu>
2017-11-27 20:53:29 -0500

    Minimal data needed for CreateChoreForm

--- cleanslate/doma/forms.py
+++ cleanslate/doma/forms.py
@@ -6,3 +6,29 @@ class EditChoreForm(forms.Form):
     def clean_deadline(self):
         data = self.cleaned_data['deadline']
         return data
+
+class CreateChoreForm(forms.Form):
+    title = forms.CharField(help_text = 'Enter a chore name')
+    description = forms.CharField(help_text = 'Enter a description')
+    created_on = forms.DateField()
+    deadline = forms.DateField(help_text = 'When is this chore due?')
+
+    # no longer than 200
+    def clean_title(self):
+        data = self.cleaned_data['title']
+        return data
+
+    # no longer than 500
+    def clean_description(self):
+        data = self.cleaned_data['description']
+        return data
+    
+    # can't be in the future
+    def clean_created_on(self):
+        data = self.cleaned_data['created_on']
+        return data
+
+    # can't be in the past
+    def clean_deadline(self):
+        data = self.cleaned_data['deadline']
+        return data
