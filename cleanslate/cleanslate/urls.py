b2ec409eebc5f5da57a7584ebe26b5917488ad23
Molly O'Neil <mboneil@umass.edu>
2017-11-27 20:45:31 -0500

    Setup urls for creating chores

--- cleanslate/cleanslate/urls.py
+++ cleanslate/cleanslate/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls import include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
-from doma.views import home, profile, calendar, reminders, finance, edit_chore_deadline
+from doma.views import home, profile, calendar, reminders, finance, edit_chore_deadline, create_chore_deadline
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -30,6 +30,7 @@ urlpatterns = [
     url(r'^doma/finance/$', finance, name='finance'),
     url(r'^doma/calendar/$', calendar, name='calendar'),
     url(r'^doma/chore/(?P<pk>[-\w]+)/edit/$', edit_chore_deadline, name = 'edit-chore-deadline'),
+    url(r'^doma/chore/create/$', create_chore_deadline, name = 'create-chore-deadline'),
 ]
 
 urlpatterns += [
