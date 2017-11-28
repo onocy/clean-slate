7008e71e82b1e278b239bcb5e42ee71085bca56e
Molly O'Neil <mboneil@umass.edu>
2017-11-27 20:29:51 -0500

    Forgot to remove unnecessary url in wrong file

--- cleanslate/doma/urls.py
+++ cleanslate/doma/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^chore/(?P<pk>[-\w]+)/edit/$', views.edit_chore_deadline, name = 'edit-chore-deadline'),
 ]
